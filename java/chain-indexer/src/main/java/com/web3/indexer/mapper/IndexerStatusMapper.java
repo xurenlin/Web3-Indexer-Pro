23188872a2720fcccad57d8f26735c62565ff442
徐韧麟 <1774091872@qq.com>
2025-10-29 23:06:46 +0800

    feat: 更新 MyBatis-Plus 依赖，移除不必要的配置，优化 IndexerStatusMapper 接口,解决spring boot3 与mybatis plus 冲突的问题，提升plus的版本

--- java/chain-indexer/src/main/java/com/web3/indexer/mapper/IndexerStatusMapper.java
+++ java/chain-indexer/src/main/java/com/web3/indexer/mapper/IndexerStatusMapper.java
@@ -1,18 +1,17 @@
 package com.web3.indexer.mapper;
 
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.web3.indexer.model.dto.IndexerStatusResponse;
 
-import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 import org.apache.ibatis.annotations.Update;
 
-@Mapper
-public interface IndexerStatusMapper {
+// 不要使用 @Mapper 注解
+public interface IndexerStatusMapper extends BaseMapper<IndexerStatusResponse> {
 
     @Select("SELECT * FROM indexer_status WHERE chain_id = #{chainId}")
-    public IndexerStatusResponse getIndexerStatus(String chainId);
+    IndexerStatusResponse getIndexerStatus(String chainId);
 
-    @Update("UPDATE indexer_status SET status = #{status}, update_time = #{lastUpdated} WHERE chain_id = #{chainId}")
+    @Update("UPDATE indexer_status SET status = #{status}, update_time = #{updateTime} WHERE chain_id = #{chainId}")
     void updateIndexerStatus(IndexerStatusResponse status);
-
 }
